5d76fdcc273451cc10e9a401b19f95ce20a19b20
gyakkun <xiaowufeixia@gmail.com>
2022-09-09 09:02:58 +0800

    update

--- src/jvmMain/kotlin/Mht2Html.kt
+++ src/jvmMain/kotlin/Mht2Html.kt
@@ -1,24 +1,24 @@
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.consume
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newFixedThreadPoolContext
+import kotlinx.coroutines.runBlocking
 import org.apache.commons.codec.binary.Base64
 import org.apache.commons.imaging.Imaging
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.RandomAccessFile
-import java.util.*
-import kotlin.random.Random
 
 object Mht2Html {
     lateinit var raf: RandomAccessFile
     lateinit var BOUNDARY: String
-    val offsetOfNextPartChannel = Channel<Triple<UUID, Long, Long>>()
-    val FILE_LOCATION = "U:\\[U2CSM][BD外挂字幕群].mht"
-    val IMG_OUTPUT_PATH = "E:\\U2CSM_Groupchat_History_From_161123\\TEST"
+    val FILE_LOCATION = "U:\\纯洁的DD隔离病院.mht"
+    val IMG_OUTPUT_PATH = "A:\\byr_img"
+    val THREAD_COUNT = 6
+    val tp = newFixedThreadPoolContext(THREAD_COUNT, "mth2html")
 
-    fun job() {
+    fun job() = runBlocking(tp) {
         var timing: Long = System.currentTimeMillis()
         raf = RandomAccessFile(FILE_LOCATION, "r")
         var fileOffset: Long = -1
@@ -40,72 +40,59 @@ object Mht2Html {
 
         System.err.println(BOUNDARY)
         val sunday = Sunday(raf, 0L, BOUNDARY.toByteArray())
-        var counter = 0
-        var limit = 10
-        var nextOffset = -1L
-        val offsetList: MutableList<Long> = ArrayList()
-        // val base64Decoder = Base64.
-        while (/*counter < limit &&*/ (sunday.getNextOffSet().also { nextOffset = it } > 0L)) {
-            offsetList.add(nextOffset)
-            val ls = offsetList.size
-            if (ls > 2) {
-                GlobalScope.launch {
+
+        val producer = produceOffSet(sunday, ArrayList())
+        repeat(THREAD_COUNT) {
+            launchConsumer(it,/*latch,*/ producer)
+        }
+        timing = System.currentTimeMillis() - timing
+        System.err.println("TOTAL:  Timing: $timing ms")
+    }
+
+    private fun CoroutineScope.launchConsumer(id: Int, channel: ReceiveChannel<Triple<Long, Long, String>>) = launch {
+        val lRaf = RandomAccessFile(FILE_LOCATION, "r")
+        for (msg in channel) {
+            val beginOffsetOfB64 = msg.first
+            val endOffsetOfB64 = msg.second
+            val uuid = msg.third
+            val b64Len = endOffsetOfB64 - beginOffsetOfB64
+            val ba = ByteArray(b64Len.toInt())
+            lRaf.seek(beginOffsetOfB64)
+            lRaf.read(ba)
+            val decode = Base64.decodeBase64(ba)
+            val fileExt = Imaging.guessFormat(decode).extension
+
+            val fos = FileOutputStream(File(IMG_OUTPUT_PATH).resolve("$uuid.$fileExt"))
+            fos.write(decode)
+            fos.flush()
+            fos.close()
+        }
+    }
+
+    private fun CoroutineScope.produceOffSet(sunday: Sunday, offsetList: ArrayList<Long>) =
+        produce {
+            val lRaf = RandomAccessFile(FILE_LOCATION, "r")
+            var nextOffset: Long
+            while (/*counter < limit &&*/ (sunday.getNextOffSet().also { nextOffset = it } > 0L)) {
+                offsetList.add(nextOffset)
+                val ls = offsetList.size
+                if (ls > 2) {
                     val offset = offsetList[ls - 2]
-                    val thisScope = offsetList[ls - 1] - offsetList[ls - 2]
                     var line: String = ""
-                    var tmpRaf = RandomAccessFile(FILE_LOCATION, "r")
                     var uuid: String = "INVALID"
-                    tmpRaf.seek(offset)
-                    while (tmpRaf.readLine().also { line = it } != null) {
+                    lRaf.seek(offset)
+                    while (lRaf.readLine().also { line = it } != null) {
                         if (line.contains("Content-Location")) {
                             uuid = line.substring("Content-Location:{".length, line.indexOf("}.dat"))
-                            System.err.println(uuid)
-                            tmpRaf.readLine()
+                            // System.err.println(uuid)
+                            lRaf.readLine()
                             break
                         }
                     }
-
-//                    val sb = StringBuffer()
-//                    while (tmpRaf.readLine().also { line = it }.isNotBlank()) {
-//                        sb.append(line)
-//                    }
-
-                val beginOffsetOfB64 = tmpRaf.filePointer
-                val endOffsetOfB64 = offsetList[ls - 1]
-                val b64Len = endOffsetOfB64 - beginOffsetOfB64
-                val ba = ByteArray(b64Len.toInt())
-                tmpRaf.seek(beginOffsetOfB64)
-                tmpRaf.read(ba)
-                val decode = Base64.decodeBase64(ba)
-                val baos = ByteArrayOutputStream()
-                baos.write(decode)
-                val fileExt = Imaging.guessFormat(decode).extension
-
-//                    val decode = base64Decoder.decode(sb.toString())
-//                    val baos = ByteArrayOutputStream()
-//                    baos.write(decode)
-//                    val fileExt = Imaging.guessFormat(decode).extension
-
-                    val fos = FileOutputStream(imgOutputPath.resolve("$uuid.$fileExt"))
-                    fos.write(decode)
-                    fos.flush()
-                    fos.close()
+                    val beginOffsetOfB64 = lRaf.filePointer
+                    val endOffsetOfB64 = offsetList[ls - 1]
+                    send(Triple(beginOffsetOfB64, endOffsetOfB64, uuid))
                 }
             }
-            counter++
         }
-        timing = System.currentTimeMillis() - timing
-        System.err.println("TOTAL: $counter offset detected. Timing: $timing ms")
-    }
-
-    fun consumer() {
-        offsetOfNextPartChannel.consume {
-
-        }
-    }
-
-    suspend fun producer() {
-        offsetOfNextPartChannel.send(Triple(UUID.randomUUID(), Random.nextLong(), Random.nextLong()))
-    }
-
 }
