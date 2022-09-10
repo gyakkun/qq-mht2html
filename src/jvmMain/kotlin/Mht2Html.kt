ee849f79c2efc1223bd3f8281e3131c8bd4ecadd
gyakkun <xiaowufeixia@gmail.com>
2022-09-10 11:09:38 +0800

    Handling first line.

--- src/jvmMain/kotlin/Mht2Html.kt
+++ src/jvmMain/kotlin/Mht2Html.kt
@@ -8,9 +8,16 @@ import org.apache.commons.imaging.Imaging
 import java.io.File
 import java.io.FileOutputStream
 import java.io.RandomAccessFile
+import java.lang.RuntimeException
 import java.lang.StringBuilder
+import java.nio.charset.Charset
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.CountDownLatch
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+import kotlin.text.Charsets.UTF_8
 
 @ExperimentalMaterialApi
 @ExperimentalCoroutinesApi
@@ -54,7 +61,7 @@ object Mht2Html {
             imgOutputFolder.mkdirs()
         }
         var line: String
-        while (raf.readLine().also { line = it } != null) {
+        while (raf.readLineInUtf8().also { line = it } != null) {
             if (line.contains("boundary=\"")) {
                 BOUNDARY = "--" + line.substring(line.indexOf("=") + 2, line.length - 1)
                 fileOffset = raf.filePointer
@@ -88,22 +95,39 @@ object Mht2Html {
 
 //        for (latch in latchList) latch.await()
         showInfoBar(showAlert, errMsg, "Processing HTML...", 5_000L)
-        processHtml(fileLocation, ArrayList<Long>().apply { add(sunday.getNextOffSet()) }, fileOutputPath)
+        processHtml(
+            fileLocation,
+            ArrayList<Long>().apply { add(sunday.getNextOffSet()) },
+            fileOutputPath,
+            imgOutputPath
+        )
         val timingMsg = "TOTAL: Timing: ${System.currentTimeMillis() - timing} ms"
         showInfoBar(showAlert, errMsg, timingMsg, 60_000L)
     }
 
-    private fun processHtml(fileLocation: String, offsetList: java.util.ArrayList<Long>, fileOutputPath: String) {
-        val dateRegex = Regex("日期 (\\d{4}-\\d{2}-\\d{2})")
+    val DATE_REGEX = Regex(".*日期: (\\d{4}-\\d{2}-\\d{2}).*")
+    fun RandomAccessFile.readLineInUtf8() = run {
+        val rawLine: String = readLine()
+        String(rawLine.toByteArray(Charset.forName("ISO-8859-1")), UTF_8)
+    }
+
+    private fun processHtml(
+        fileLocation: String,
+        offsetList: java.util.ArrayList<Long>,
+        fileOutputPath: String,
+        imgOutputPath: String
+    ) {
         val styleClassNameMap = ConcurrentHashMap<String, String>()
         var lineCounter = 0
         val endOfFile = "</table></body></html>"
         val raf = RandomAccessFile(fileLocation, "r")
+        val imgOutputFolder = File(imgOutputPath)
+        val imgFileNameExtensionMap: Map<String, String> = getImgFileNameExtensionMap(imgOutputFolder)
         val firstBoundaryOffset = offsetList[0]
         raf.seek(firstBoundaryOffset)
         var line: String
         var firstLineOffset = -1L
-        while (raf.readLine().also { line = it } != null) {
+        while (raf.readLineInUtf8().also { line = it } != null) {
             if (line.isEmpty()) {
                 firstLineOffset = raf.filePointer
                 break;
@@ -111,8 +135,20 @@ object Mht2Html {
         }
         var counter = 0
         raf.seek(firstLineOffset)
+        val firstLine = raf.readLineInUtf8()
+        val (firstLineWithStylePlaceHolder, htmlHeadTemplate, globalStyleSheet, startDateInUTC, dateLineWithPlaceHolder) = handleFirstLine(
+            firstLine,
+            styleClassNameMap
+        )
+        System.err.println("/////////////////")
+        System.err.println(firstLineWithStylePlaceHolder)
+        System.err.println(htmlHeadTemplate)
+        System.err.println(globalStyleSheet)
+        System.err.println(startDateInUTC.toInstant().toString())
+        System.err.println(dateLineWithPlaceHolder)
+
         while (counter++ < 10) {
-            val line = raf.readLine()
+            val line = raf.readLineInUtf8()
             extractAndReplaceStyle(line, styleClassNameMap)
         }
         // TODO: Extract the first line
@@ -121,6 +157,83 @@ object Mht2Html {
         // Lazy load of image
         // Date handling
         // Paging
+    }
+
+    private fun getImgFileNameExtensionMap(imgOutputFolder: File): Map<String, String> {
+        if (!imgOutputFolder.isDirectory) {
+            throw RuntimeException("Image output path is not a folder!")
+        }
+        val m = imgOutputFolder.listFiles().groupBy({ it.nameWithoutExtension }, { it.extension })
+        val result = HashMap<String, String>()
+        for (e in m.entries) {
+            result[e.key] = e.value[0]
+        }
+        return result
+    }
+
+    private const val STYLE_PLACEHOLDER = "#STYLE_PLACEHOLDER"
+    private const val DATE_PLACEHOLDER = "#DATE_PLACEHOLDER"
+    private val YYYY_MM_DD_DATE_FORMATTER_UTC =
+        SimpleDateFormat("yyyy-MM-dd").apply { timeZone = TimeZone.getTimeZone("UTC") }
+
+    private fun handleFirstLine(
+        rawFirstLine: String,
+        styleClassNameMap: MutableMap<String, String>
+    ): ExtractFirstLineResult {
+        val firstLine = extractAndReplaceStyle(rawFirstLine, styleClassNameMap)
+        val closingTitleTag = "</title>"
+        val closingStyleTag = "</style>"
+        val beforeDate = "&nbsp;</div></td></tr>"
+        val closingDateTag = "</td></tr>"
+        var sb = StringBuilder()
+        val startOfStyleTag = firstLine.indexOf(closingTitleTag) + closingTitleTag.length
+        val endOfStyleTagPlusOne = firstLine.indexOf(closingStyleTag, startOfStyleTag) + closingStyleTag.length
+        val styleWithTag = firstLine.substring(startOfStyleTag, endOfStyleTagPlusOne)
+        val globalStyleSheet =
+            styleWithTag.substring("<style type=\"text/css\">".length, styleWithTag.length - closingStyleTag.length)
+        val startOfDateCell = firstLine.indexOf(beforeDate) + beforeDate.length
+        val endOfDateCellPlusOne = firstLine.indexOf(closingDateTag, startOfDateCell) + closingDateTag.length
+        val dateCell = firstLine.substring(startOfDateCell, endOfDateCellPlusOne)
+        System.err.println("**** DATE CELL $dateCell")
+        sb.append(firstLine.substring(0, startOfStyleTag))
+        sb.append("\r\n")
+        sb.append("<style type=\"text/css\">$STYLE_PLACEHOLDER</style>")
+        sb.append(firstLine.substring(endOfStyleTagPlusOne, startOfDateCell))
+
+
+        assert(DATE_REGEX.matches(firstLine))
+        var startDate = Date()
+        var startDateStr = ""
+        if (DATE_REGEX.matches(firstLine)) {
+            startDateStr = extractDateFromLine(firstLine).first
+            startDate = extractDateFromLine(firstLine).second
+        }
+
+        val dateLineWithPlaceHolder = dateCell.replace(startDateStr, DATE_PLACEHOLDER)
+        sb.append("\r\n")
+        sb.append(dateLineWithPlaceHolder)
+        val htmlHeadTemplate = sb.toString()
+        sb.append(firstLine.substring(endOfDateCellPlusOne))
+        return ExtractFirstLineResult(
+            sb.toString().replace(DATE_PLACEHOLDER, startDateStr),
+            htmlHeadTemplate,
+            globalStyleSheet,
+            startDate,
+            dateLineWithPlaceHolder
+        )
+    }
+
+    data class ExtractFirstLineResult(
+        val convertedFirstLine: String,
+        val htmlHeadTemplate: String,
+        val globalStyleSheet: String,
+        val date: Date,
+        val dateLine: String
+    )
+
+    private fun extractDateFromLine(firstLine: String): Pair<String, Date> {
+        val yyyyMmDd = DATE_REGEX.find(firstLine)!!.groupValues[1]
+        return Pair(yyyyMmDd, YYYY_MM_DD_DATE_FORMATTER_UTC.parse(yyyyMmDd))
     }
 
     private fun extractAndReplaceStyle(line: String, styleClassNameMap: MutableMap<String, String>): String {
@@ -215,7 +328,7 @@ object Mht2Html {
                 var line: String
                 var uuid = "INVALID"
                 raf.seek(offset)
-                while (raf.readLine().also { line = it } != null) {
+                while (raf.readLineInUtf8().also { line = it } != null) {
                     if (line.contains("Content-Location")) {
                         uuid = line.substring("Content-Location:{".length, line.indexOf("}.dat"))
                         // System.err.println(uuid)
